3cd115f1184a1fc73e015a9398686a09284ca99b
Giovanni Perez <frozen@Giovannis-MacBook-Pro.local>
2017-08-14 15:45:58 -0400

    fuck

--- mac/setup-pf.sh
+++ mac/setup-pf.sh
@@ -4,7 +4,7 @@ if [ "$EUID" -ne 0 ]
 fi
 
 chmod -R +x *.sh
-chmod -R +x *.plist
+
 
 echo "Removing Apple's PF rules and replacing with frozen.pf.rules"
 mv /etc/pf.anchors/com.apple ~/Desktop/com.apple.anchors.backup
@@ -12,15 +12,15 @@ cp com.frozen.pf.rules /etc/pf.anchors/com.frozen.pf.rules
 
 echo "Replacing pf.conf"
 mv /etc/pf.conf ~/Desktop/pf.conf.backup
-cp pf.conf /etc/pf.conf
+sudo cp pf.conf /etc/pf.conf
 
 echo "Removing default pfctl plist, adding pf startup script"
 mv /System/Library/LaunchDaemons/com.apple.pfctl.plist ~/Desktop/pfctl-plist.backup
-cp com.apple.pfctl.plist /System/Library/LaunchDaemons/com.apple.pfctl.plist
-cp com.frozen.pf-startup.plist /System/Library/LaunchDaemons/com.frozen.pf.plist
+sudo cp com.apple.pfctl.plist /System/Library/LaunchDaemons/com.apple.pfctl.plist
+sudo cp com.frozen.pf-startup.plist /Library/LaunchDaemons/com.frozen.pf.plist
 
-chmod +x /System/Library/LaunchDaemons/com.apple.pfctl.plist
-chmod +x /System/Library/LaunchDaemons/com.frozen.pf.plist
+chmod 644 /Library/LaunchDaemons/com.frozen.pf.plist
+chown root:wheel /Library/LaunchDaemons/com.frozen.pf.plist
 
-echo "Rebooting"
-reboot 
+echo "Please restart your machine"
+# reboot 
